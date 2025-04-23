c967b443490c5f5dcc0d8a06cccdfd15f5c95b41
MichaelKrampah <163937878+MichaelKrampah@users.noreply.github.com>
2025-04-23 16:03:00 +0000

    Added new components

--- components/Header.jsx
+++ components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
         {/* logo */}
         <Link href="/">
          <h1 className="text-4xl font-semibold">
-            MICHAEL KRAMPAH <span className="text-accent">.</span>
+            MICKY <span className="text-accent">.</span>
          </h1>
         </Link>
 
